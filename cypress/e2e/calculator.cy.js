049b091d95b49a883b05bf71b81ddb4d8bc0790b
Naefunn <nathan.d.foster1@gmail.com>
2022-08-19 23:01:26 +0100

    PDA

---
+++ cypress/e2e/calculator.cy.js
@@ -0,0 +1,59 @@
+describe("Calculator", () => {
+
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  })
+
+  it('Do the number buttons update the display of the running total?', () => {
+    cy.get('#number2').click();
+    cy.get('.display').should('contain', '2')
+  })
+
+  it('Do the arithmetical operations update the display with the result of the operation?', () => {
+    cy.get('#number6').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '12')
+  })
+
+  it('Can chain multiple operations together', () => {
+    cy.get('#number6').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number2').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '6')
+  })
+
+  it('Can display decimal numbers', () => {
+    cy.get('#number5').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '2.5')
+  })
+
+  it('Can display large numbers', () => {
+    cy.get('#number5').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number1').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '50000')
+  })
+
+  it('Can handle unusual circumstances ( dividing by 0 )', () => {
+    cy.get('#number5').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number0').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '0')
+  })
+
+
+})
